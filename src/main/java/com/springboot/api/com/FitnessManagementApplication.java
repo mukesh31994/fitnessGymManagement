fbbcbebbeb5a1606610313de3e9080aa49c2917b
unknown <mukeshsharma31994@gmail.com>
2021-12-08 14:00:30 +0000

    project creation

---
+++ src/main/java/com/springboot/api/com/FitnessManagementApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.api.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessManagementApplication.class, args);
+	}
+
+}
