c7cfa8d39afdb735a006c84dafc8906ebbdb4366
unknown <10574812@mydbs.ie>
2021-12-11 03:50:43 +0530

    js added

--- src/main/webapp/js/viewConfig.js
+++ src/main/webapp/js/viewConfig.js
@@ -62,6 +62,121 @@ var memberList = {
 	]
 };
 
+
+
+var instructorList = {
+	columns: [
+		{
+			mDataProp: 'instructorId',
+			title: 'ID',
+			type: 'number',
+			width: '20',
+			//			sortable: true,
+
+		},
+		{
+			mDataProp: 'instructorName',
+			title: 'Name',
+			type: 'string',
+			width: '20',
+			render: function(pValue, meta, record) {
+				debugger;
+				var reqStr = '<a  href="#ui/form/UpdateInstructor.html?id=' + record.instructorId + '" id="' + pValue + '" type="ajax">' + pValue + '</a>';
+				return reqStr;
+			}
+			//			sortable: true
+		},
+		{
+			mDataProp: 'contact',
+			title: 'contact',
+			type: 'number',
+			width: '20',
+			//			sortable: true
+		},
+		{
+			mDataProp: 'email',
+			title: 'email',
+			type: 'string',
+			width: '20',
+			//			sortable: true,
+			//            render: dateFormat
+		}
+	]
+};
+
+
+var workoutList = {
+	columns: [
+		{
+			mDataProp: 'workoutId',
+			title: 'ID',
+			type: 'number',
+			width: '20'
+		},
+		{
+			mDataProp: 'workoutName',
+			title: 'workout Name',
+			type: 'string',
+			width: '20',
+			render: function(pValue, meta, record) {
+				debugger;
+				var reqStr = '<a  href="#ui/form/UpdateWorkout.html?id=' + record.workoutId + '" id="' + pValue + '" type="ajax">' + pValue + '</a>';
+				return reqStr;
+			}
+		},
+		{
+			mDataProp: 'description',
+			title: 'description',
+			type: 'string',
+			width: '20'
+		}
+	]
+};
+
+var workoutPlanList = {
+	columns: [
+		{
+			mDataProp: 'planId',
+			title: 'ID',
+			type: 'number',
+			width: '20'
+		},
+		{
+			mDataProp: 'memberId',
+			title: 'Member Id',
+			type: 'number',
+			width: '20',
+			render: function(pValue, meta, record) {
+				debugger;
+				var reqStr = '<a  href="#ui/form/UpdateWorkoutPlan.html?id=' + record.planId + '" id="' + pValue + '" type="ajax">' + pValue + '</a>';
+				return reqStr;
+			}
+		},
+		{
+			mDataProp: 'workoutId',
+			title: 'Workout Id',
+			type: 'string',
+			width: '20',
+			render: function(pValue, meta, record) {
+				debugger;
+				return workoutJson[pValue];
+			}
+		},
+		{
+			mDataProp: 'workoutDate',
+			title: 'Date',
+			type: 'date',
+			width: '20'
+		},
+		{
+			mDataProp: 'instructorId',
+			title: 'instructor Id',
+			type: 'number',
+			width: '20'
+		}
+	]
+};
+
 var attendanceList = {
 	columns: [
 		{
@@ -111,5 +226,8 @@ var paymentList = {
 var viewConfig = {
 	"memberList": memberList,
 	"attendance": attendanceList,
-	"paymentList": paymentList
+	"paymentList": paymentList,
+	"instructorList":instructorList,
+	"workoutList":workoutList,
+	"workoutPlanList":workoutPlanList
 };
