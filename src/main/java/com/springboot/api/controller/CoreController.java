2b4d385d0fe589ea894787fbc91bef8f60680c53
unknown <10574812@mydbs.ie>
2021-12-11 04:01:26 +0530

    controller modified

--- src/main/java/com/springboot/api/controller/CoreController.java
+++ src/main/java/com/springboot/api/controller/CoreController.java
@@ -12,33 +12,47 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.api.model.Attendance;
+import com.springboot.api.model.Instructor;
 import com.springboot.api.model.Member;
 import com.springboot.api.model.Payment;
+import com.springboot.api.model.Workout;
+import com.springboot.api.model.WorkoutPlan;
 import com.springboot.api.service.AttendanceService;
+import com.springboot.api.service.InstructorService;
 import com.springboot.api.service.MemberService;
 import com.springboot.api.service.PaymentService;
-
+import com.springboot.api.service.WorkoutPlanService;
+import com.springboot.api.service.WorkoutService;
 
 //import com.google.gson.Gson;
 //import com.management.common.model.User_Master;
 
 @RestController
 public class CoreController {
-	
+
 	@Autowired
 	MemberService memberService;
-	
+
 	@Autowired
 	AttendanceService attendanceService;
 
 	@Autowired
 	PaymentService paymentService;
+
+	@Autowired
+	InstructorService instructorService;
+
+	@Autowired
+	WorkoutService workoutService;
 	
+	@Autowired
+	WorkoutPlanService workoutPlanService;
+
 	@GetMapping("welcome")
 	public String getMessage() {
 		return "Welcome to Azure Web App for Containers ";
 	}
-	
+
 	@RequestMapping(value = "/addMember", method = RequestMethod.POST)
 	public @ResponseBody String addMember(Member lMember) {
 		lMember.setJoining_date(new java.sql.Date(Calendar.getInstance().getTime().getTime()));
@@ -46,7 +60,7 @@ public class CoreController {
 
 		return "success";
 	}
-	
+
 	@RequestMapping(value = "/getAllMember", method = RequestMethod.POST)
 	public List<Member> getAllMember() {
 		return memberService.findAll();
@@ -56,19 +70,19 @@ public class CoreController {
 	public Member getByMemberId(@RequestParam(value = "memberId") int memberId) {
 		return memberService.findByMemberId(memberId);
 	}
-	
+
 	@RequestMapping(value = "/addAttendance", method = RequestMethod.POST)
 	public @ResponseBody String addAttendance(Attendance lAttendance) {
 		lAttendance.setDate(new java.sql.Date(Calendar.getInstance().getTime().getTime()));
 		attendanceService.saveAttendance(lAttendance);
 		return "success";
 	}
-	
+
 	@RequestMapping(value = "/getAttendanceByMemberId", method = RequestMethod.POST)
 	public List<Attendance> getAttendanceByMemberId(@RequestParam(value = "memberId") int memberId) {
 		return attendanceService.findByMemberId(memberId);
 	}
-	
+
 	@RequestMapping(value = "/addPayment", method = RequestMethod.POST)
 	public @ResponseBody String addPayment(Payment lPayment) {
 		lPayment.setPaymentDate(new java.sql.Date(Calendar.getInstance().getTime().getTime()));
@@ -76,10 +90,66 @@ public class CoreController {
 		paymentService.savePayment(lPayment);
 		return "success";
 	}
-	
+
 	@RequestMapping(value = "/getPaymentByMemberId", method = RequestMethod.POST)
 	public List<Payment> getPaymentByMemberId(@RequestParam(value = "memberId") int memberId) {
 		return paymentService.findByMemberId(memberId);
+	}
+
+	/* -----------Instructor Controller----------- */
+
+	@RequestMapping(value = "/addInstructor", method = RequestMethod.POST)
+	public @ResponseBody String addMember(Instructor lInstructor) {
+		lInstructor.setUserId(1);
+		instructorService.saveInstructor(lInstructor);
+		return "success";
+	}
+
+	@RequestMapping(value = "/getAllInstructor", method = RequestMethod.POST)
+	public List<Instructor> getAllInstructor() {
+		return instructorService.findAll();
+	}
+
+	@RequestMapping(value = "/getByInstructorId", method = RequestMethod.POST)
+	public Instructor getByInstructorId(@RequestParam(value = "instructorId") int instructorId) {
+		return instructorService.findByInstructorId(instructorId);
+	}
+
+	/* -----------Workout Controller----------- */
+
+	@RequestMapping(value = "/addWorkout", method = RequestMethod.POST)
+	public @ResponseBody String addWorkout(Workout lWorkout) {
+		workoutService.saveWorkout(lWorkout);
+		return "success";
+	}
+
+	@RequestMapping(value = "/getAllWorkout", method = RequestMethod.POST)
+	public List<Workout> getAllWorkout() {
+		return workoutService.findAll();
+	}
+
+	@RequestMapping(value = "/getByWorkoutId", method = RequestMethod.POST)
+	public Workout getByWorkoutId(@RequestParam(value = "workoutId") int workoutId) {
+		return workoutService.findByWorkoutId(workoutId);
+	}
+	
+	
+	/* -----------Workout Plan Controller----------- */
+
+	@RequestMapping(value = "/addWorkoutPlan", method = RequestMethod.POST)
+	public @ResponseBody String addWorkoutPlan(WorkoutPlan lWorkoutPlan) {
+		workoutPlanService.saveWorkoutPlan(lWorkoutPlan);
+		return "success";
+	}
+
+	@RequestMapping(value = "/getWorkoutPlanByMemberId", method = RequestMethod.POST)
+	public List<WorkoutPlan> getWorkoutPlanByMemberId(@RequestParam(value = "memberId") int memberId) {
+		return workoutPlanService.findByMemberId(memberId);
+	}
+	
+	@RequestMapping(value = "/getByPlanId", method = RequestMethod.POST)
+	public WorkoutPlan getByPlanId(@RequestParam(value = "planId") int planId) {
+		return workoutPlanService.findByPlanId(planId);
 	}
 
 }
