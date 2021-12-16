9874a200860ccb41ff9b43dc335c1a88aef42a91
unknown <mukeshsharma31994@gmail.com>
2021-12-16 22:15:47 +0000

    code changes for email

--- src/main/java/com/springboot/api/utility/SendEmailTLS.java
+++ src/main/java/com/springboot/api/utility/SendEmailTLS.java
@@ -1,5 +1,83 @@
 package com.springboot.api.utility;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import java.util.Properties;
+
 public class SendEmailTLS {
+
+    public static void main(String[] args) {
+
+        final String username = "mukeshsharma31994@gmail.com";
+        final String password = "sjxfssqcgqygsape";
+
+        Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("mukeshsharma31994@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("mukeshsharma31994@gmail.com")
+            );
+            message.setSubject("Testing Gmail TLS");
+            
+            ///////////////////////////////////////
+            
+         // This mail has 2 part, the BODY and the embedded image
+            MimeMultipart multipart = new MimeMultipart("related");
+
+            // first part (the html)
+            BodyPart messageBodyPart = new MimeBodyPart();
+            String htmlText = "<H1>Hello</H1><img src=\"cid:image\">";
+            messageBodyPart.setContent(htmlText, "text/html");
+            // add it
+            multipart.addBodyPart(messageBodyPart);
+
+            // second part (the image)
+            messageBodyPart = new MimeBodyPart();
+            DataSource fds = new FileDataSource(
+               "/home/manisha/javamail-mini-logo.png");
+
+            messageBodyPart.setDataHandler(new DataHandler(fds));
+            messageBodyPart.setHeader("Content-ID", "<image>");
+
+            // add image to the multipart
+            multipart.addBodyPart(messageBodyPart);
+
+            // put everything together
+            message.setContent(multipart);
+            
+            ///////////////////////////////////////
+//            message.setText("Dear Mail Crawler Testing,"
+//                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
