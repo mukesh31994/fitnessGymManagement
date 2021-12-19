094e3618c037096d16dbb5111852fcab51dd7777
Alison Barretto <10574812@mydbs.ie>
2021-12-19 01:06:18 +0000

    branchmaster service added in repository

--- src/main/java/com/springboot/api/repository/BranchRepository.java
+++ src/main/java/com/springboot/api/repository/BranchRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.springboot.api.model.Branchmaster;
+import com.springboot.api.model.Member;
 
 @Repository
 public interface BranchRepository extends JpaRepository<Branchmaster, Integer> {
 	
+	 public Branchmaster findByBranchId(int pbranchId);
 }
